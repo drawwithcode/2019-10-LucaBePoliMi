4c6792aab0763ece29c6b2db202cf2efbd53723c
LucaBePoliMi <luca.b97@gmail.com>
2019-12-07 23:17:47 +0100

    Commented code, added icon

--- sketch.js
+++ sketch.js
@@ -1,65 +1,79 @@
+// Initializing variables for creategraphics
 var sign;
 var signW;
 var signH;
+
+// html element
 var reference;
+
+// and image
 var maoAndyImg;
 
 function preload() {
 
-maoAndyImg = loadImage("./assets/MaoAndy.jpg");
+  maoAndyImg = loadImage("./assets/MaoAndy.jpg");
 
 }
 
 function setup() {
 
-signW = 500;
-signH = 500;
+  // setting variables for width and height of creategraphics
+  signW = 500;
+  signH = 500;
 
   createCanvas(windowWidth, windowHeight, WEBGL);
   sign = createGraphics(signW, signH);
-  sign.textSize(75);
 
+  // setting html element which will contain a link
   reference = select("#referenceDiv");
-  reference.position(1/28 * windowWidth, 13/14 * windowHeight);
+  reference.position(1 / 28 * windowWidth, 13 / 14 * windowHeight);
   reference.addClass("reference");
 }
 
 function draw() {
   background(0);
+
+  // setting the previous image as background
   sign.background(maoAndyImg);
+
+  // creating a 50% opacity rectangle to ensure readability
   sign.push();
   sign.noStroke();
   sign.fill(0, 0, 0, 127);
-sign.rect(0, 0, 500, 500);
+  sign.rect(0, 0, 500, 500);
   sign.pop();
 
+  // creating instructions
   sign.fill(255);
   sign.textFont("Trebuchet MS");
   sign.textSize(25);
   sign.push();
   sign.fill(255, 200, 30);
-  sign.text("Press M to go to the model", signW/6, signH/6);
+  sign.text("Press M to go to the model", signW / 6, signH / 6);
   sign.pop();
-  sign.text("You will be able to change colors by moving your mouse or to swap them by clicking", signW/6, signH * 2/6, 4/6 * signW, 600);
-sign.text("Reference: Mao by Andy Warhol", signW/6, signH * 5/6);
+  sign.text("You will be able to change colors by moving your mouse or to swap them by clicking", signW / 6, signH * 2 / 6, 4 / 6 * signW, 600);
+  sign.text("Inspired to Mao by Andy Warhol", signW / 6, signH * 5 / 6);
 
-var cameraX = mouseX - width/2;
-var cameraY = mouseY - height/2;
-camera(cameraX, cameraY, -600, 0, 0, 0, 1, 0, -1);
+  //setting the camera for the instructions cube
+  var cameraX = mouseX - width / 2;
+  var cameraY = mouseY - height / 2;
+  camera(cameraX, cameraY, -600, 0, 0, 0, 1, 0, -1);
 
+  // creating the instructions cube with the creategraphics and so its contents as texture
   texture(sign);
   rotateX(1);
   rotateY(40)
   translate(0, 30, 0);
   noStroke();
-  box(height/2, height/2, height/2);
+  box(height / 2, height / 2, height / 2);
 }
 
+// pressing M will allow to open the page with the 3d model of Mao
 function keyTyped() {
   if (key == "m") {
 
-window.open("indexMao.html", "_self");
+    window.open("indexMao.html", "_self");
 
-}
+  }
 
 }
--- sketchMao.js
+++ sketchMao.js
@@ -1,185 +1,159 @@
-// https://publicdelivery.org/andy-warhols-mao/
-
+// Initializing variables for different colored states
 var state1 = true;
 var state2 = false;
-var canvas;
-var pencil = false;
 
-function preload(){
-mask = loadModel("assets/minimumMao.obj");
+
+function preload() {
+  // loading the 3d model of Mao
+  mao = loadModel("assets/minimumMao.obj");
 }
 
+// setting variable for creategraphics
 var maoSurface;
-var sign;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-angleMode(DEGREES);
+  angleMode(DEGREES);
 
-maoSurface = createGraphics(windowWidth, windowHeight);
-maoSurface.background(255, 255, 255);
-
-sign = createGraphics(500, 500);
-sign.fill(0);
-sign.textAlign(CENTER);
-sign.text("aaaaaaaa", 150, 150);
-
-textFont("Trebuchet MS");
-textSize(16);
-textAlign(CENTER, CENTER);
+  maoSurface = createGraphics(windowWidth, windowHeight);
+  // the background is 255 so that all the colors can be enlightened by light sources
+  // the colors will be given infact by light sources, not by the background color of the surface
+  // this way the 3d model can be colored by many colors of any value the mouse x and y will generate
+  // while if I used the mouse x and y directly to generate the background color
+  // the lights could shine of just a limited range of rgb values, limit given by the background color
+  maoSurface.background(255, 255, 255);
 }
 
+// principal variables used to map color
 var maxColorValue = 255;
 var minColorValue = 0;
 
 function draw() {
 
-// Assigning width for mouseY or height for mouseX as values in map() avoids creating grey color
+  // Assigning width for mouseY or height for mouseX as values in map() avoids creating grey color
+  // because this way the rgb values won't ever be the all same
 
-if (state1 == true && state2 == false) {
-// background which changes with user mouse movement
-background(
-  map(mouseX, 0, width, maxColorValue, minColorValue)
-  ,
-   map(mouseY, 0, height, maxColorValue, minColorValue)
-   ,
-    map(mouseX, 0, height, minColorValue, maxColorValue)
-  );
-
-
-
-
-// face color light
-directionalLight(
-  map(mouseY, 0, height, maxColorValue, minColorValue)
-  ,
-   map(mouseX, 0, width, minColorValue, maxColorValue)
-   ,
-    map(mouseX, 0, height, minColorValue, maxColorValue)
-    , -(width/5 / width - 0.5) * 2, -(2* height / height - 0.5) * 2, -1);
-
-
-// Chest color light
-pointLight(
-
-  map(mouseX, 0, width, minColorValue, maxColorValue)
-    ,
-     map(mouseX, 0, width, maxColorValue, minColorValue)
-     ,
-    map(mouseY, 0, height, maxColorValue, minColorValue)
-
-  , 0, -70, 80);
-
-// Giving Mao a color which changes with user mouse movement
-// Main light
-
-directionalLight(
-  map(mouseX, 0, height, minColorValue, maxColorValue)
-  ,
-   map(mouseY, 0, height, minColorValue, maxColorValue)
-   ,
-    map(mouseX, 0, height, maxColorValue, minColorValue)
-, 1, 0.5, 0);
-
-// Secondary light, smoothens shadows
-directionalLight(
-  map(mouseX, 0, width, minColorValue, maxColorValue)
-  ,
-   map(mouseY, 0, height, minColorValue, maxColorValue)
-   ,
-    map(mouseX, 0, height, maxColorValue, minColorValue)
-, -1, 0, 0);
-
-} // end of if statement for state 1
-
-
-else if (state1 == false && state2 == true) {
-
-  background(
-    map(  mouseX, 0, width, minColorValue, maxColorValue)
-    ,
-     map(mouseY, 0, height, minColorValue, maxColorValue)
-     ,
+  // colors state 1, standard
+  if (state1 == true && state2 == false) {
+    // background
+    background(
+      map(mouseX, 0, width, maxColorValue, minColorValue),
+      map(mouseY, 0, height, maxColorValue, minColorValue),
       map(mouseX, 0, height, minColorValue, maxColorValue)
     );
 
-// face color light
-directionalLight(
-  map(mouseX, 0, height, minColorValue, maxColorValue)
-    ,
-     map(mouseY, 0, height, maxColorValue, minColorValue)
-     ,
-    map(mouseY, 0, height, maxColorValue, minColorValue)
-    , -(width/5 / width - 0.5) * 2, -(2* height / height - 0.5) * 2, -1);
-
-
-// Chest color light
-pointLight(
-
-  map(mouseX, 0, width, maxColorValue, minColorValue
-    )
-  ,
-   map(mouseX, 0, width, minColorValue, maxColorValue)
-   ,
-    map(mouseX, 0, height, minColorValue, maxColorValue)
-
-  , 0, -70, 80);
-
-// Giving Mao a color which changes with user mouse movement
-// Main light
-
-directionalLight(
-  map(mouseX, 0, height, minColorValue, maxColorValue)
-  ,
-   map(mouseY, 0, height, maxColorValue, minColorValue)
-   ,
-    map(mouseX, 0, width, maxColorValue, minColorValue)
-, 1, 0.5, 0);
-
-// Secondary light, smoothens shadows
-directionalLight(
-  map(mouseX, 0, height, minColorValue, maxColorValue)
-  ,
-   map(mouseY, 0, height, maxColorValue, minColorValue)
-   ,
-    map(mouseX, 0, width, maxColorValue, minColorValue)
-, -1, 0, 0);
-
-} // end of if statement for state 2
 
 
 
-scale(3);
-translate(0, 50, 0);
-rotateZ(180);
-//rotateY(-6);
-rotateX(10);
-noStroke();
+    // face color light
+    directionalLight(
+      map(mouseY, 0, height, maxColorValue, minColorValue),
+      map(mouseX, 0, width, minColorValue, maxColorValue),
+      map(mouseX, 0, height, minColorValue, maxColorValue), -(width / 5 / width - 0.5) * 2, -(2 * height / height - 0.5) * 2, -1);
 
 
-model(mask);
-texture(maoSurface);
+    // Chest color light
+    pointLight(
+
+      map(mouseX, 0, width, minColorValue, maxColorValue),
+      map(mouseX, 0, width, maxColorValue, minColorValue),
+      map(mouseY, 0, height, maxColorValue, minColorValue), 0, -70, 80);
+
+    // Giving Mao the main color with lights
+    // Main light
+    directionalLight(
+      map(mouseX, 0, height, minColorValue, maxColorValue),
+      map(mouseY, 0, height, minColorValue, maxColorValue),
+      map(mouseX, 0, height, maxColorValue, minColorValue), 1, 0.5, 0);
+
+    // Secondary light, smoothens shadows
+    directionalLight(
+      map(mouseX, 0, width, minColorValue, maxColorValue),
+      map(mouseY, 0, height, minColorValue, maxColorValue),
+      map(mouseX, 0, height, maxColorValue, minColorValue), -1, 0, 0);
+
+  } // end of if statement for state 1
+
+  // color state 2, after click
+  else if (state1 == false && state2 == true) {
+
+    background(
+      map(mouseX, 0, width, minColorValue, maxColorValue),
+      map(mouseY, 0, height, minColorValue, maxColorValue),
+      map(mouseX, 0, height, minColorValue, maxColorValue)
+    );
+
+    // face color light
+    directionalLight(
+      map(mouseX, 0, height, minColorValue, maxColorValue),
+      map(mouseY, 0, height, maxColorValue, minColorValue),
+      map(mouseY, 0, height, maxColorValue, minColorValue), -(width / 5 / width - 0.5) * 2, -(2 * height / height - 0.5) * 2, -1);
+
+
+    // Chest color light
+    pointLight(
+
+      map(mouseX, 0, width, maxColorValue, minColorValue),
+      map(mouseX, 0, width, minColorValue, maxColorValue),
+      map(mouseX, 0, height, minColorValue, maxColorValue), 0, -70, 80);
+
+    // Giving Mao the main colors with lights
+    // Main light
+
+    directionalLight(
+      map(mouseX, 0, height, minColorValue, maxColorValue),
+      map(mouseY, 0, height, maxColorValue, minColorValue),
+      map(mouseX, 0, width, maxColorValue, minColorValue), 1, 0.5, 0);
+
+    // Secondary light, smoothens shadows
+    directionalLight(
+      map(mouseX, 0, height, minColorValue, maxColorValue),
+      map(mouseY, 0, height, maxColorValue, minColorValue),
+      map(mouseX, 0, width, maxColorValue, minColorValue), -1, 0, 0);
+
+  } // end of if statement for state 2
+
+
+  // enlarging the 3d model
+  scale(3);
+
+  translate(0, 50, 0);
+
+  // to have him not upside down
+  rotateZ(180);
+
+  //rotateY(-6); apply this to have him looking at you
+
+  // slightly inclined, less monumental, more faithful to the reference
+  rotateX(10);
+
+  //avoiding black borders contaminating the colors
+  noStroke();
+
+  // rendering the model
+  model(mao);
+
+  // applying the model the white creategraphics as texture
+  texture(maoSurface);
 
 }
-
+// clicking will swap colors
 function mouseClicked() {
 
-if(state1 == true && state2 == false) {
+  if (state1 == true && state2 == false) {
 
-state2 = true;
-state1 = false;
+    state2 = true;
+    state1 = false;
 
 
-}
+  } else if (state1 == false && state2 == true) {
 
-else if (state1 == false && state2 == true) {
-
-state2 = false;
-state1 = true;
+    state2 = false;
+    state1 = true;
 
 
 
-}
+  }
 
 
 
